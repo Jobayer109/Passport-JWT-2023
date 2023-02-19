e721952e82d04facf71c328ac42827f5aa688034
user.email <jobayer.ahmed109@gmail.com>
2023-02-20 00:12:23 +0600

    bla bla bla

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -1,82 +1,10 @@
-const User = require("../models/user.model");
-const bcrypt = require("bcrypt");
-const saltRounds = 10;
-const jwt = require("jsonwebtoken");
-require("../config/passport");
-
 // Register
-const registerUser = async (req, res) => {
-  try {
-    const { username, password } = req.body;
-
-    const user = await User.findOne({ username });
-    if (user) return res.status(404).send("user already exist");
-
-    bcrypt.hash(password, saltRounds, async (err, hash) => {
-      const newUser = new User({
-        username: username,
-        password: hash,
-      });
-      await newUser
-        .save()
-        .then((user) =>
-          res.send({
-            success: true,
-            message: "user created successfully",
-            user: {
-              id: user._id,
-              username: user.username,
-            },
-          })
-        )
-        .catch((error) => {
-          res.send({
-            success: false,
-            message: "user isn't created",
-            error: error.message,
-          });
-        });
-    });
-  } catch (error) {
-    res.status(404).send({
-      success: false,
-      message: "user doesn't created",
-    });
-  }
-};
+// const registerUser =
 
 // Login user.
-const loginUser = async (req, res) => {
-  const { username, password } = req.body;
-  const user = await User.findOne({ username });
+// const loginUser =
 
-  if (!user)
-    return res.status(404).send({
-      success: false,
-      message: " user not found",
-    });
-
-  if (!bcrypt.compareSync(password, user.password)) {
-    return res.status(404).send({
-      success: false,
-      message: "Password is incorrect",
-    });
-  }
-
-  // token implementation
-  const payload = {
-    id: user._id,
-    username: user.username,
-  };
-  const token = jwt.sign(payload, process.env.privateKey, { expiresIn: "1d" });
-  return res.send({
-    success: true,
-    message: "user is logged in successfully",
-    token: "Bearer" + token,
-  });
-};
-
-module.exports = {
-  registerUser,
-  loginUser,
-};
+// module.exports = {
+//   registerUser,
+//   loginUser,
+// };
--- server/index.js
+++ server/index.js
@@ -3,11 +3,13 @@ const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 5000;
 const dbConnect = require("./config/database");
-const { registerUser, loginUser } = require("./controllers/user.controller");
 const passport = require("passport");
 require("./config/database");
 require("./config/passport");
-require("dotenv").config();
+const User = require("./models/user.model");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
+const jwt = require("jsonwebtoken");
 
 // middleware
 app.use(cors());
@@ -23,10 +25,76 @@ app.get("/", (req, res) => {
 });
 
 // Register
-app.post("/register", registerUser);
+app.post("/register", async (req, res) => {
+  try {
+    const { username, password } = req.body;
+
+    const user = await User.findOne({ username });
+    if (user) return res.status(404).send("user already exist");
+
+    bcrypt.hash(password, saltRounds, async (err, hash) => {
+      const newUser = new User({
+        username: username,
+        password: hash,
+      });
+      await newUser
+        .save()
+        .then((user) =>
+          res.send({
+            success: true,
+            message: "user created successfully",
+            user: {
+              id: user._id,
+              username: user.username,
+            },
+          })
+        )
+        .catch((error) => {
+          res.send({
+            success: false,
+            message: "user isn't created",
+            error: error.message,
+          });
+        });
+    });
+  } catch (error) {
+    res.status(404).send({
+      success: false,
+      message: "user doesn't created",
+    });
+  }
+});
 
 // Login
-app.post("/login", loginUser);
+app.post("/login", async (req, res) => {
+  const { username, password } = req.body;
+  const user = await User.findOne({ username });
+
+  if (!user)
+    return res.status(404).send({
+      success: false,
+      message: " user not found",
+    });
+
+  if (!bcrypt.compareSync(password, user.password)) {
+    return res.status(404).send({
+      success: false,
+      message: "Password is incorrect",
+    });
+  }
+
+  // token implementation
+  const payload = {
+    id: user._id,
+    username: user.username,
+  };
+  const token = jwt.sign(payload, process.env.privateKey, { expiresIn: "1d" });
+  return res.status(200).send({
+    success: true,
+    message: "user is logged in successfully",
+    token: "Bearer" + token,
+  });
+});
 
 // Profile (protected)
 app.get("/profile", passport.authenticate("jwt", { session: false }), (req, res) => {
